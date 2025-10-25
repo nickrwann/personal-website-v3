a8cb5492739f6bedd2410759fe57c6bc81a6e97d
Nick Wanner <nickrwann@gmail.com>
2025-10-25 15:07:32 -0500

    Fix Replit Env Issues

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,11 @@
+
+import 'dotenv/config';
+import dotenv from "dotenv";
+dotenv.config();
+
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-
-import dotenv from "dotenv";
-dotenv.config();
 
 const app = express();
 
